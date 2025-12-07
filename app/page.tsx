6ec0a0c80a4957f56660a638aa2b6ecb0c25a829
sestacik <samuel.sestak11@gmail.com>
2025-12-07 08:45:47 +0100

    first version

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+export default function Home() {
+  return (
+    <main>
+      <h1>hi</h1>
+      
+ <button onClick={() => alert("GOOOOOD BOY :)")}>
+  Click me
+</button>
+    </main>
+  )
+}
