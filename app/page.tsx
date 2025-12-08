7bb469d24d801e1e2e653b01be3b56afc730aa0f
ll887854 <gtasestacik@gmail.com>
2025-12-08 17:50:14 +0100

    ver3 page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,19 @@
 
 import { useState } from "react"
 
-export default function Home () {
+export default function Home() {
   const [number, setNumber] = useState(0)
 
   return (
     <main>
       <h1>Score: {number}</h1>
-      <button onClick={() => setNumber(number +1)}>
+
+      <button onClick={() => setNumber(number + 1)}>
         +1
       </button>
-      {number >= 10 && number < 20 && <keep going!</p>}
-     {number >= 20 && number < 30 && <p>almost there</p>}
+
+      {number >= 10 && number < 20 && <p>keep going</p>}
+      {number >= 20 && number < 30 && <p>almost there</p>}
       {number >= 30 && <p>YOU DID IT!</p>}
     </main>
   )
