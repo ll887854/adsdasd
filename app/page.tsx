f4206eaabd85bcf6b6f1a6c57536d064ec1ef111
ll887854 <gtasestacik@gmail.com>
2025-12-08 15:47:59 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 "use client"
 
-export default function Home() {
+import { useState } from "react"
+
+export default function Home () {
+  const [number, setNumber] = useState(0)
+
   return (
     <main>
-      <h1>hi</h1>
-      
- <button onClick={() => alert("GOOOOOD BOY :)")}>
-  Click me
-</button>
+      <h1>Score: {number}</h1>
+      <button onClick={() => setNumber(number +1)}>
+        +1
+      </button>
+      {number >= 10 && number < 20 && <p>keep going PUSSY!</p>}
+     {number >= 20 && number < 30 && <p>10 MORE NOW!</p>}
+      {number >= 30 && <p>GOOOOD BOY YOU DID IT!</p>}
     </main>
   )
 }
