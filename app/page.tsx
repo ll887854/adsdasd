6b6354ec0bfadf67bd7ef57e759b056eda5f4c50
ll887854 <gtasestacik@gmail.com>
2025-12-10 18:12:03 +0100

    e page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,65 @@
+
 "use client"
-
 import { useState } from "react"
+export default function Home () {
+  const [level, setLevel] = useState("start")
+    const [number, setNumber] = useState(0)
+      const [text, setText] = useState("PLAY MY GAME!!!!")
 
-export default function Home() {
-  const [number, setNumber] = useState(0) 
-  const [text, setText] = useState("click!!")
+      return (
+        <main>
+          {level === "start" && 
+          ( <div> <h1>Wanna play the Click Game Ultra Sonic Clicker Man??</h1>
+             <button onClick={() => setLevel("playing")}>
+              Yes
+             </button> 
+             <div>
+             <button onClick={() => setLevel("fuck you")}>
+              No
+             </button>
+             </div>
+             </div>
+          )
+        }
 
-  return (
-    <main>
-      <h1>Score: {number}</h1>
+        {level === "fuck you" &&
+          ( <div>
+            <h1>{text}</h1>
+            <button onClick={() => setLevel("playing")}>
+              PLAY!
+            </button>
+          </div>
+          )
+        }
 
-      <button onClick={() => setNumber(number + 1)}>
-        +1
-      </button>
+    
 
-      {number >= 10 && number < 20 && <p>keep going</p>}
-      {number >= 20 && number < 30 && <p>almost there</p>}
-      {number >= 30 && <p>YOU DID IT!</p>}
-    </main>
-  )
-}
+        {level === "playing" && (
+          <div>
+            <h1>Score: {number}</h1>
+            
+            <button onClick={() => setNumber(number + 30)}>
+              click!
+            </button>
+            <div>
+
+            {number >= 100 && ( <button onClick={() => setLevel("finish")}>
+              Oki you can go now just click me 
+            </button> )}
+            </div>
+          </div>
+        )
+      }
+      {level === "finish" && 
+      ( <div>
+        <h1>YAY! YOU DID IT!</h1>
+   
+      </div>
+      )
+    }
+
+        
+      
+        </main>
+      )
+    }
